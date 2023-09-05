ae91b0d6b0acd010533d307018cf68aa2c858bf5
Marwan Elgendy <marwantarek852@gmail.com>
2023-09-05 23:59:53 +0300

    - Create Trending destination, Hotels , Apartments and Email subscription sections - edit the reservation card component to use dynamic data

--- src/Components/Reservation_Card/reservationCard.js
+++ src/Components/Reservation_Card/reservationCard.js
@@ -2,38 +2,38 @@ import React, { useState } from 'react';
 import hotel from '../../Imgs/hotel.jpg'
 import './reservationCard.css'
 
-const ReservationCard = () => {
+const ReservationCard = ({reservation}) => {
 
     const[viewMore , setViewMore] = useState(false)
 
     const description = "Located in Destin, 90 metres from Miramar Beach and 2.8 km from James Lee Park Public Beach, Majestic Sun 202B offers air conditioning. Featuring sea and lake views, this holiday home also provides guests with free WiFi. Featuring a DVD player, the holiday home has a kitchen with a dishwasher, an oven and a microwave, a living room with a seating area and a dining area, 2 bedrooms, and 2 bathrooms with a bath and a shower. Towels and bed linen are featured in the holiday home."
     
     const shorterDescription = (description)=>{
-        return description.substring(0,300) + "..."
+        return description.substring(0,250) + "..."
     }
     return (
-        <div className='reservation-card'>
+        <div key={reservation._id} className='reservation-card'>
 
-            <img src={hotel} alt="hotel" className="reserv-img" />
+            <img src={reservation.images[0]} alt="hotel" className="reserv-img" />
             <div className="info-container">
-                <h3 className="name">Majestic Sun</h3>
-                <p className="location"> Destin, 32550, United States</p>
+                <h3 className="name">{reservation.title}</h3>
+                <p className="location"> {reservation.location}</p>
                 <div className="review">
-                    <p className="rate">9.5</p>
+                    <p className="rate">{reservation.rating}</p>
                     <p className="no-reviews">20 reviews</p>
                 </div>
                 <p className="description">
                     {
                         !viewMore && 
                                 <>
-                                    {shorterDescription(description)}
+                                    {shorterDescription(reservation.description[1])}
                                     <span className='view-more-less-btn' onClick={()=> setViewMore(true)}>more</span>
                                 </>
                     }
                     {
                         viewMore &&
                             <>
-                                {description}
+                                {reservation.description}
                                 <span className='view-more-less-btn' onClick={()=> setViewMore(false)}>less</span>
                             </>
                     }
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,29 +1,150 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './home.css'
+import ReservationCard from '../../Components/Reservation_Card/reservationCard';
 
 import plane from '../../Imgs/plane.png'
 import places from '../../Imgs/places.png'
+import dest1 from '../../Imgs/dest1.jpg'
+import dest2 from '../../Imgs/dest2.jpg'
+import dest3 from '../../Imgs/dest3.jpg'
+import dest4 from '../../Imgs/dest4.jpeg'
+import dest5 from '../../Imgs/dest5.jpg'
+import saFlage from '../../Imgs/sa-flag.png'
+import uaeFlage from '../../Imgs/uae-flag.png'
+import engFlage from '../../Imgs/engFlag.png'
+import frFlage from '../../Imgs/frFlag.png'
+import axios from 'axios';
 
 
 const Home = () => {
+    const [hotels, setHotels] = useState([])
+    const [apartments, setApartments] = useState([])
+
+    useEffect(() => {
+        axios.get("http://localhost:3000/gethotels", { params: { len: 4 } })
+            .then(response => {
+                console.log(response.data)
+                setHotels(response.data)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+
+        axios.get("http://localhost:3000/getappartments", { params: { len: 4 } })
+            .then(response => {
+                setApartments(response.data)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }, [])
     return (
         <div className='home'>
-            <div className='content'>
-                <h2>Discover The Best Of The World</h2>
-                <h3>Let’s Book Your Next Adventure</h3>
-                <div className='feature'>
-                    <p>Esay</p>
-                    <p>.</p>
-                    <p>Fast</p>
-                    <p>.</p>
-                    <p>Comfortable</p>
+
+            {/* Landing Section */}
+            <div className="landing">
+                <div className='content'>
+                    <h2>Discover The Best Of The World</h2>
+                    <h3>Let’s Book Your Next Adventure</h3>
+                    <div className='feature'>
+                        <p>Esay</p>
+                        <p>.</p>
+                        <p>Fast</p>
+                        <p>.</p>
+                        <p>Comfortable</p>
+                    </div>
+                </div>
+                <div className="photos">
+                    <img className='plane-ph' src={plane} alt="plane" />
+                    <img className='places-ph' src={places} alt="places" />
                 </div>
             </div>
-            <div className="photos">
-                <img className='plane-ph' src={plane} alt="plane" />
-                <img className='places-ph' src={places} alt="places" />
+
+            {/* Trending Destination */}
+            <div className='trending-destination-section'>
+                <h2 className='title'>Trending Destinations</h2>
+                <div className="destinations-container">
+                    <div className="destination-first-row">
+                        <div className="destination-item">
+                            <img src={dest1} alt="" className="destination-img" />
+                            <div className="destination-name">
+                              <p>Riyadh</p>  
+                              <img src={saFlage} alt="" />
+                            </div>
+                        </div>
+                        <div className="destination-item">
+                            <img src={dest2} alt="" className="destination-img" />
+                            <div className="destination-name">
+                                <p>Dubai</p>
+                                <img src={uaeFlage} alt="" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="destination-second-row">
+                        <div className="destination-item">
+                            <img src={dest3} alt="" className="destination-img" />
+                            <div className="destination-name">
+                                <p>Makkah</p>
+                                <img src={saFlage} alt="" />
+                            </div>
+                        </div>
+                        <div className="destination-item">
+                            <img src={dest4} alt="" className="destination-img" />
+                            <div className="destination-name">
+                                <p>London</p>
+                                <img src={engFlage} width={24} height={16} alt="" />
+                            </div>
+                        </div>
+                        <div className="destination-item">
+                            <img src={dest5} alt="" className="destination-img" />
+                            <div className="destination-name">
+                                <p>Paris</p>
+                                <img src={frFlage} width={24} height={16} alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
+            {/* Hotels Section */}
+            <div className="hotels-section">
+                <h2 className="title">Hotels</h2>
+                <div className="hotels-container">
+                    <p className="view-all">View All</p>
+                    <div className="hotel-items">
+                        {
+                            hotels.map(hotel => (
+                                <ReservationCard reservation={hotel} />
+                            ))
+                        }
+                    </div>
+                </div>
+            </div>
+
+            {/* Apartment Section */}
+            <div className="apartments-section">
+                <h2 className="title">Apartments</h2>
+                <div className="apartments-container">
+                    <p className="view-all">View All</p>
+                    <div className="apartment-items">
+                        {
+                            apartments.map(apartment => (
+                                <ReservationCard reservation={apartment} />
+                            ))
+                        }
+                    </div>
+                </div>
+            </div>
+
+            {/* Email Subscription Section */}
+            <div className='email-sub-section'>
+                <h2>Save time, save money!</h2>
+                <p>Sign up and we'll send the best deals to you</p>
+                <div className="input-container">
+                    <input type="email" name="email" id="email" placeholder='Your email address' />
+                    <button className="btn-contain subscribe-btn">Subscribe</button>
+                </div>
+            </div>
         </div>
     );
 }
